792cce9bae99bd6b7dca718d0497e226a06ba060
NyoByte <20173527@aloe.ulima.edu.pe>
2021-09-15 01:50:41 -0500

    Card en medio avanzado

--- app/src/main/java/pe/edu/ulima/game_8_locos/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/game_8_locos/MainActivity.kt
@@ -2,10 +2,14 @@ package pe.edu.ulima.game_8_locos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.ulima.game_8_locos.views.CardTableView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fviCardTable = findViewById<CardTableView>(R.id.fviCardTable)
+        fviCardTable.ratio = 1.5f
     }
 }
